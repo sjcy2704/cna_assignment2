5b991981f0fe5da3bdd7a6930c16d5dab9c9a13f
sjcy2704 <a1834160@adelaide.edu.au>
2025-05-05 16:17:25 +0930

    Implemented A_input to handle ACKs, mark packets as acknowledged, slide base, and manage global timer in SR - Included a debug print to make sure the variables are being updated properly

--- sr.c
+++ sr.c
@@ -52,6 +52,8 @@ void A_output(struct msg message)
   // calculate current window size: how many unACKed packets are in-flight.
   // use modulo to handle sequence number wrap-around correctly.
   int window_size = (A_nextseqnum + SEQSPACE - A_base) % SEQSPACE;
+  printf("A_output: window_size = %d, A_base = %d, A_nextseq = %d\n",
+    window_size, A_base, A_nextseqnum);
   if (window_size >= WINDOWSIZE) {
     // If the window is full, drop the message (i.e., don't send it).
     if (TRACE > 0) {
@@ -108,6 +110,62 @@ void A_output(struct msg message)
 */
 void A_input(struct pkt packet)
 {
+  // check if packet is corrupted
+  if (IsCorrupted(packet)) {
+    if (TRACE > 0)
+        printf("----A: Corrupted ACK %d received, ignored\n", packet.acknum);
+    return;
+  }
+
+  total_ACKs_received++;
+  int acknum = packet.acknum;
+
+  // we need to only handle the ACKs for packets that are currently in the sender's window 
+  // if packet is already acknowledge, then is a duplicate ACK
+  if (!A_ackeds[acknum]) {
+      A_ackeds[acknum] = true;
+      A_expiries[acknum] = -1.0; // Stop simulated timer for this packet
+      new_ACKs++;
+
+      if (TRACE > 0)
+          printf("----A: ACK %d received and marked as acknowledged\n", acknum);
+  } else {
+      if (TRACE > 0)
+          printf("----A: Duplicate ACK %d received, ignored\n", acknum);
+      return;
+  }
+
+  // in selective repeat, the sender window base moves forward only if the base packet (A_base) has been acknowledged
+  // because the window is circular (going back to 0), we must use modulo to handle then wrap cleanly
+  // we continue sliding the base forward until we find the first unACKed packet
+  while (A_ackeds[A_base]) {
+      A_ackeds[A_base] = false;          // reset slot for reuse
+      A_expiries[A_base] = -1.0;         // cancel timer
+      A_base = (A_base + 1) % SEQSPACE;  // slide the base forward, the modulo ensures that it wraps back to 0
+  }
+
+  // the emulator only allows ONE real timer, so we simulate per-packet timers with a 1-second global tick
+  // if any unACKed packets remain in the window, we keep the timer running. if all are ACKed, we can stop it.
+  bool has_unacked = false;
+  // check if there is a packet with active timers
+  for (int i = 0; i < WINDOWSIZE; i++) {
+      int index = (A_base + i) % SEQSPACE;
+
+      if (A_expiries[index] > 0) {
+          has_unacked = true;
+          break;
+      }
+  }
+
+  // start timer if there are unACKed packets and timer is not active
+  if (has_unacked && !A_timer_is_active) {
+      starttimer(A, 1.0);
+      A_timer_is_active = true;
+  } else if (!has_unacked) {
+      // stop timer if all packets are ACKed
+      stoptimer(A);
+      A_timer_is_active = false;
+  }
 }
 
 /* called when A's timer goes off */
@@ -147,12 +205,55 @@ static int B_nextseqnum;   /* the sequence number for the next packets sent by B
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
+  struct pkt sendpkt;
+  int i;
+
+  /* if not corrupted and received packet is in order */
+  if  ( (!IsCorrupted(packet))  && (packet.seqnum == expectedseqnum) ) {
+    if (TRACE > 0)
+      printf("----B: packet %d is correctly received, send ACK!\n",packet.seqnum);
+    packets_received++;
+
+    /* deliver to receiving application */
+    tolayer5(B, packet.payload);
+
+    /* send an ACK for the received packet */
+    sendpkt.acknum = expectedseqnum;
+
+    /* update state variables */
+    expectedseqnum = (expectedseqnum + 1) % SEQSPACE;
+  }
+  else {
+    /* packet is corrupted or out of order resend last ACK */
+    if (TRACE > 0)
+      printf("----B: packet corrupted or not expected sequence number, resend ACK!\n");
+    if (expectedseqnum == 0)
+      sendpkt.acknum = SEQSPACE - 1;
+    else
+      sendpkt.acknum = expectedseqnum - 1;
+  }
+
+  /* create packet */
+  sendpkt.seqnum = B_nextseqnum;
+  B_nextseqnum = (B_nextseqnum + 1) % 2;
+
+  /* we don't have any data to send.  fill payload with 0's */
+  for ( i=0; i<20 ; i++ )
+    sendpkt.payload[i] = '0';
+
+  /* computer checksum */
+  sendpkt.checksum = ComputeChecksum(sendpkt);
+
+  /* send out packet */
+  tolayer3 (B, sendpkt);
 }
 
 /* the following routine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init(void)
 {
+  expectedseqnum = 0;
+  B_nextseqnum = 1;
 }
 
 /******************************************************************************
